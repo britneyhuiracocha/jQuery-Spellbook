296cb331fb21d72a1d2bd6408a48739745ab5e95
britneyhuiracocha <43241766+britneyhuiracocha@users.noreply.github.com>
2019-03-07 09:32:46 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,12 @@
+    $("#trick1st").click(function(){
+    $(".trickone").toggle();
+});
+
+    $("#trick2nd").click(function(){
+    $(".secondtrick").fadeOut();
+    $("#secret").show();
+});
+    $("#trick3rd").click(function(){
+    $(".thirdtrick").hide();
+});
+
